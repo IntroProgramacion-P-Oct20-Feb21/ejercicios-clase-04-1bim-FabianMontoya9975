efa9b25ccd4eece9a6c510b804719ff3604a1d2b
Fabián Montoya <jfmontoya1@utpl.edu.ec>
2020-11-04 10:51:55 -0500

    ejemplo04

--- Ejemplo01/src/ejemplo01/Ejemplo05.java
+++ Ejemplo01/src/ejemplo01/Ejemplo05.java
@@ -12,9 +12,9 @@ package ejemplo01;
 public class Ejemplo05 {
     public static void main(String[] args) {
         // Secuencia de escape \n y \t
-        String nombreEstudiante = "René Rolando";
-        String apellidoEstudiante = "Elizalde Solano";
-        int nacimiento = 1983;
+        String nombreEstudiante = "José Fabián";
+        String apellidoEstudiante = "Montoya Montoya";
+        int nacimiento = 2002;
         
         System.out.println(nombreEstudiante+"\n"+apellidoEstudiante+ "\n"+ nacimiento);   
         System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
